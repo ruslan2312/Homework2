a3630a1d206c5c231be03d012e13b434270a1238
ruslan2312 <heeca@mail.ru>
2022-09-25 15:26:18 +0300

    FirstCommit

--- src/Router/blogs-router.ts
+++ src/Router/blogs-router.ts
@@ -6,7 +6,7 @@ import {BlogsRepository} from "../Repository/blogs-repository";
 export const BlogsRouter = Router()
 
 const nameValidation = body('name').trim().isLength({min: 1, max: 15})
-const youtubeUrlValidation = body('youtubeUrl').trim().isLength({min: 1, max: 100})
+const youtubeUrlValidation = body('youtubeUrl').isURL().isLength({min: 1, max: 100})
 
 BlogsRouter.get('/', (req: Request, res: Response) => {
     const findBlogs = BlogsRepository.findBlog(req.query.name?.toString())
