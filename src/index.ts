10cca34d611f726a4cdbdb3af3a9d1fc23ab4974
ruslan2312 <heeca@mail.ru>
2022-10-01 20:05:16 +0300

    Homework part 2

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import express from "express"
 import bodyParser from "body-parser";
 import {BlogsRouter} from "./Router/blogs-router";
 import {PostsRouter} from "./Router/posts-router";
-import {mwBasicAuth} from "./Middleware/authorization-middleware";
-import basicAuth from "express-basic-auth";
 
 const app = express();
 const port = process.env.PORT || 3000
 
 app.use(bodyParser())
-app.use('/bloggers', BlogsRouter)
+app.use('/blogs', BlogsRouter)
 app.use('/posts', PostsRouter)
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
