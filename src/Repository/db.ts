4820d309cd1b0808493164029bb601da635951c8
ruslan2312 <heeca@mail.ru>
2022-10-07 18:28:35 +0300

    Homework part 3

--- src/Repository/db.ts
+++ src/Repository/db.ts
@@ -2,9 +2,11 @@ import {MongoClient} from "mongodb"
 import {BlogsType} from "./blogs-repository";
 import {PostsType} from "./posts-Repository";
 
-const mongoUri = process.env.mongoURI || "mongodb://0.0.0.0:27017"
+const mongoUri = process.env.mongoURI || "mongodb+srv://146.70.108.10/32 "
 
 const client = new MongoClient(mongoUri)
+
+
 const db = client.db("Profile")
 export const BlogsCollection = db.collection<BlogsType>("blogs")
 export const PostsCollection = db.collection<PostsType>("posts")
