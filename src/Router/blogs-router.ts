b0b9b7d6410d872692f4a123ba93ad4530b87600
ruslan2312 <heeca@mail.ru>
2022-10-12 18:47:47 +0300

    Homework part 4 AddPagitation

--- src/Router/blogs-router.ts
+++ src/Router/blogs-router.ts
@@ -20,7 +20,7 @@ const youtubeUrlValidation = body('youtubeUrl').isURL().isLength({min: 1, max: 1
 const getPaginationData = (query: any): PaginationQueryType => {
     const searchNameTerm = query.searchNameTerm ? query.searchNameTerm : "";
     const pageSize = isNaN(query.pageSize) ? 10 : query.pageSize;
-    const pageNumber = isNaN(query.page) ? 1 : query.page;
+    const pageNumber = isNaN(query.pageNumber) ? 1 : query.pageNumber;
     const sortBy = query.sortBy === "name" ? "name" : "createdAt";
     const sortDirection = query.sortDirection === "asc" ? "asc" : "desc";
     return {searchNameTerm, pageSize, pageNumber, sortBy, sortDirection}
