765acad23073a654d19a48c467b0d94d14be62b6
ruslan2312 <heeca@mail.ru>
2022-10-12 21:31:40 +0300

    Homework part 4 AddPagitation

--- src/Repository/blogs-repository.ts
+++ src/Repository/blogs-repository.ts
@@ -11,7 +11,7 @@ export const BlogsRepository = {
             filter.name = { $regex: queryData.searchNameTerm, $options: 'i'}
         }
         debugger
-         const totalCount = await BlogsCollection.countDocuments({name:  {$regex: queryData.searchNameTerm , $options: 'i'}})
+         const totalCount = await BlogsCollection.countDocuments({name:  filter.name})
         const pagesCount = Number(Math.ceil(totalCount / queryData.pageSize))
         const page = Number(queryData.pageNumber)
         const pageSize = Number(queryData.pageSize)
