aaddff696d6dfda174e4a3f36b5147ff40074678
ruslan2312 <heeca@mail.ru>
2022-10-15 00:28:37 +0300

    Homework part 4 AddValidator

--- src/Common/GetBlogPaginationData.ts
+++ src/Common/GetBlogPaginationData.ts
@@ -9,11 +9,12 @@ export const getBlogPaginationData = (query: any): BlogPaginationQueryType => {
     return {searchNameTerm, pageSize, pageNumber, sortBy, sortDirection}
 }
 
-export const getPostPaginationData = (query: any): PostPaginationQueryType => {
+export const getPostPaginationData = (query: any): PostPaginationQueryType  => {
     const searchNameTerm = query.searchNameTerm ? query.searchNameTerm : "";
     const pageSize = isNaN(query.pageSize) ? 10 : query.pageSize
     const pageNumber = isNaN(query.pageNumber) ? 1 : query.pageNumber
-    const sortBy = query.sortBy === "blogName" ? "blogName" : "createdAt"
+    // const sortBy = query.sortBy === "blogName" ? "blogName" : "createdAt"
+    const sortBy = query.sortBy
     const sortDirection = query.sortDirection === "asc" ? "asc" : "desc";
     return {searchNameTerm, pageSize, pageNumber, sortBy, sortDirection}
 }
