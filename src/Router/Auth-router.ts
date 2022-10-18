c8f67bbd689e20b20fbb2779c0a19f9888403cd0
ruslan2312 <heeca@mail.ru>
2022-10-18 19:44:29 +0300

    5 homework

--- src/Router/Auth-router.ts
+++ src/Router/Auth-router.ts
@@ -1,8 +1,10 @@
 import {Request, Response, Router} from "express";
 import {UsersService} from "../Service/Users-service";
+import {usersEmailValidation, usersPasswordValidation} from "../Common/Validator";
+import {inputValidationMiddleware} from "../Middleware/Input-validation-middleware";
 
 export const AuthRouter = Router()
-AuthRouter.post('/login', async (req: Request, res: Response) => {
+AuthRouter.post('/login', usersEmailValidation, usersPasswordValidation, inputValidationMiddleware, async (req: Request, res: Response) => {
     debugger
     const checkResult = await UsersService.checkCredentials(req.body.login, req.body.password)
     if (checkResult) {
