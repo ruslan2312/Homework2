e2ffd450581be29882193517e301d496f8a8392f
ruslan2312 <heeca@mail.ru>
2022-10-18 13:02:07 +0300

    s

--- src/Router/Auth-router.ts
+++ src/Router/Auth-router.ts
@@ -1,15 +1,17 @@
 import {Request, Response, Router} from "express";
 import {UsersService} from "../Service/Users-service";
 import {log} from "util";
+import {usersEmailValidation, usersPasswordValidation} from "../Common/Validator";
+import {inputValidationMiddleware} from "../Middleware/Input-validation-middleware";
 
 export const AuthRouter = Router()
 
-AuthRouter.post('/login', async (req: Request, res: Response) => {
+AuthRouter.post('/login', usersEmailValidation, usersPasswordValidation, inputValidationMiddleware, async (req: Request, res: Response) => {
     const checkResult = await UsersService.checkCredentials(req.body.login, req.body.password)
     if (checkResult) {
-        res.status(204).send(req.body.login)
+        res.sendStatus(204)
     } else {
-        res.status(401).send(req.body.login)
+        res.sendStatus(401)
     }
 
 })
